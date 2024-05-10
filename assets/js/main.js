a4cfc245b7545732ace641cf385dddc31c6cab34
Alexa Ped <alexaped16@gmail.com>
2024-05-09 19:39:39 -0500

    big

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,18 +2,14 @@
 function sendEmail() {
     const name = document.getElementById('txtName').value;
     const email = document.getElementById('txtEmail').value;
-    const phone = document.getElementById('txtPhone').value;
     const company = document.getElementById('txtCompany').value;
-    const product = document.getElementById('txtProduct').value;
     const message = document.getElementById('txtMessage').value;
   
     const emailBody = `
       <h2>Contact Form submitted from Liv and Lexi</h2>
       <div>Name: <strong>${name}</strong></div>
       <div>Email: <strong>${email}</strong></div>
-      <div>Phone: <strong>${phone}</strong></div>
       <div>Company: <strong>${company}</strong></div>
-      <div>Product: <strong>${product}</strong></div>
       <div>Message: <strong>${message}</strong></div>
     `;
   
